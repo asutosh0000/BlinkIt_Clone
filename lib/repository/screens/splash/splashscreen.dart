3177498a47fa4f9e0560cbed13057ffe4163e482
Asutosh Parija <asutoshparija@Asutoshs-MacBook.local>
2026-04-23 12:08:25 +0530

    Hello

--- lib/repository/screens/splash/splashscreen.dart
+++ lib/repository/screens/splash/splashscreen.dart
@@ -1,4 +1,3 @@
-import 'package:blinkit_clone/main.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget{
--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 class UiHelper {
-  static CustomImage({required String img}){
+  static Image CustomImage({required String img}){
     return Image.asset("assets/images/$img");
   }
 }
