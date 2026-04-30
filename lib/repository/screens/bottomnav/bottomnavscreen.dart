5ffedf58ef4b5e8cc84b6a55ffa656f2a754e3d6
Asutosh Parija <asutoshparija@Asutoshs-MacBook.local>
2026-04-30 18:11:55 +0530

    Asutosh Parija

--- lib/repository/screens/bottomnav/bottomnavscreen.dart
+++ lib/repository/screens/bottomnav/bottomnavscreen.dart
@@ -1,12 +1,26 @@
+import 'package:blinkit_clone/repository/screens/cart/cartscreen.dart';
+import 'package:blinkit_clone/repository/screens/catagory/catagoryscreen.dart';
+import 'package:blinkit_clone/repository/screens/home/homescreen.dart';
+import 'package:blinkit_clone/repository/screens/print/printscreen.dart';
 import 'package:flutter/material.dart';
-
 class BottomNavScreen extends StatefulWidget{
+  const BottomNavScreen({super.key});
+
   @override
   State <BottomNavScreen> createState() => _BottomNavScreenState();
 
 }
  
 class _BottomNavScreenState extends State<BottomNavScreen>{
+
+  int currentIndex = 0;
+  List<Widget>pages=[
+    HomeScreen(),
+    CartScreen(),
+    CatagoryScreen(),
+    PrintScreen(),
+  ];
+
   @override
   Widget build(BuildContext context){
     return Scaffold();
---
+++ lib/repository/screens/catagory/catagoryscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CatagoryScreen extends StatelessWidget{
+  const CatagoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Catagory Screen"),
+      ),
+    );
+  }
+}
